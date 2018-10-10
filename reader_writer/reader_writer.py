d60765eca10d150960ca024c63905c06455c4a2b
Dylan <dylan.dvorachek@gmail.com>
2018-10-09 22:01:14 -0700

    added own reader writer solution in python

---
+++ reader_writer/reader_writer.py
@@ -0,0 +1,73 @@
+import threading
+import time
+
+class Reader(threading.Thread):
+    def __init__(self, Number):
+        threading.Thread.__init__(self)
+        self.Number = Number
+    
+    def run(self):
+        for i in range(10):
+            self.Number.read()
+
+
+
+class Writer(threading.Thread):
+    def __init__(self, Number):
+        threading.Thread.__init__(self)
+        self.Number = Number
+    
+    def run(self):
+        for i in range(10):
+
+            self.Number.write(i)
+
+class Number(object):
+    def __init__(self, start=99):
+        self.lock = threading.Lock()
+        self.value = start
+    
+    def write(self, value):
+        try:
+            self.lock.acquire()
+            print('Writer acquired the lock.')
+            self.value = value
+            print('Set value to {}.'.format(self.value))
+        except Exception as e:
+            print(e)
+        finally:
+            print('Writer released the lock.')
+            self.lock.release()
+
+    def read(self):
+        try:
+            self.lock.acquire()
+            print('Reader acquired the lock.')
+            print('Value currently equals {}.'.format(self.value))
+        except Exception as e:
+            print(e)
+        finally:
+            print('Reader released the lock.')
+            self.lock.release()
+        
+
+if __name__=='__main__':
+    number = Number()
+
+    reader1 = Reader(number)
+    reader2 = Reader(number)
+    writer = Writer(number)
+
+    reader1.start()
+
+    writer.start()
+
+    reader2.start()
+
+
+
+    reader1.join()
+    reader2.join()
+    writer.join()
+
+    print('Main terminating')
