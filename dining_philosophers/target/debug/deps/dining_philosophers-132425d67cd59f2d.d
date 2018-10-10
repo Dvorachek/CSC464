a47ccd7fea27269fbd4a8b03ee704fac6b880c3d
Dylan <dylan.dvorachek@gmail.com>
2018-10-09 18:54:07 -0700

    added rust solution by Steven Klabnik for Dining Philosophers

---
+++ dining_philosophers/target/debug/deps/dining_philosophers-132425d67cd59f2d.d
@@ -0,0 +1,5 @@
+C:\Users\Dylan\Documents\SchoolWork\CSC464\CSC464Assignment1\dining_philosophers\target\debug\deps\dining_philosophers-132425d67cd59f2d.exe: src\main.rs
+
+C:\Users\Dylan\Documents\SchoolWork\CSC464\CSC464Assignment1\dining_philosophers\target\debug\deps\dining_philosophers-132425d67cd59f2d.d: src\main.rs
+
+src\main.rs:
